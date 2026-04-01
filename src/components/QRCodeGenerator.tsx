441f17d50c97f319ce9301f47c5a00ebf16f0d5c
brodovskayariana <drariana2025@gmail.com>
2026-04-02 00:34:45 +0500

    Update QRCodeGenerator.tsx

--- src/components/QRCodeGenerator.tsx
+++ src/components/QRCodeGenerator.tsx
@@ -3,7 +3,7 @@ import { QRCodeSVG } from 'qrcode.react'
 
 const QRCodeGenerator: React.FC = () => {
   const [showQR, setShowQR] = useState(false)
-  const currentUrl = 'https://data-canal-jfb0filbf-drariana2025-labs-projects.vercel.app/'
+  const currentUrl = 'https://data-canal-iz.vercel.app/'
 
   const downloadQR = () => {
     const canvas = document.getElementById('qr-code-canvas')
