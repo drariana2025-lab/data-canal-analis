de1948302b970591e2a272b8722285cc96f774e1
brodovskayariana <drariana2025@gmail.com>
2026-04-01 21:19:32 +0500

    Update QRCodeGenerator.tsx

--- src/components/QRCodeGenerator.tsx
+++ src/components/QRCodeGenerator.tsx
@@ -3,7 +3,7 @@ import { QRCodeSVG } from 'qrcode.react'
 
 const QRCodeGenerator: React.FC = () => {
   const [showQR, setShowQR] = useState(false)
-  const currentUrl = 'http://192.168.0.106:5173'
+  const currentUrl = 'https://github.com/drariana2025-lab/data-canal-analis/pull/1'
 
   const downloadQR = () => {
     const canvas = document.getElementById('qr-code-canvas')
